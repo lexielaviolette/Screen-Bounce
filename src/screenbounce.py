88c63eec863349567aba8dbe3fe49c3f1c4f2143
lexie laviolette <lexielaviolette@hotmail.com>
2024-12-03 15:27:26 -0600

    fill screen fixed, ball disappered

--- src/screenbounce.py
+++ src/screenbounce.py
@@ -8,8 +8,16 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 
 #ball parametters
 
-
+#gameloop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:  
+           running = False
 #draw
-screen.fill((50,50,50))
+    screen.fill((50,50,50))
+    pygame.display.update()
 pygame.draw.circle(screen,(255,255,255),(screen_width//2,screen_height//2),20)
 pygame.display.flip()
+
+pygame.quit()
