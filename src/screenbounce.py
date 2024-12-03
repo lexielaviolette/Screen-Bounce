94ee95d574c927d690903e6303b0c1f3fb39303c
lexie laviolette <lexielaviolette@hotmail.com>
2024-12-03 15:14:52 -0600

    initial display, not running

--- src/screenbounce.py
+++ src/screenbounce.py
@@ -1,3 +1,13 @@
 import pygame
 
+#setup
 pygame.init()
+screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+
+#ball parametters
+
+
+#draw
+screen.fill((50,50,50))
+pygame.draw.circle(screen,(255,255,255),(screen/2,screen/2),20)
+pygame.display.flip()
