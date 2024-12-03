24906cfbfe74972a6c2cd3b270cbf7aef00caec2
lexie laviolette <lexielaviolette@hotmail.com>
2024-12-03 15:19:10 -0600

    initail display, fixed

--- src/screenbounce.py
+++ src/screenbounce.py
@@ -2,12 +2,14 @@ import pygame
 
 #setup
 pygame.init()
-screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+screen_width = 800
+screen_height = 400
+screen = pygame.display.set_mode((screen_width, screen_height))
 
 #ball parametters
 
 
 #draw
 screen.fill((50,50,50))
-pygame.draw.circle(screen,(255,255,255),(screen/2,screen/2),20)
+pygame.draw.circle(screen,(255,255,255),(screen_width//2,screen_height//2),20)
 pygame.display.flip()
